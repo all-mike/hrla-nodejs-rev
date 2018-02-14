f1e9a1f2a3796ff5d6c844703116350255d34bbe
Mike <Mdubbpro@gmail.com>
2018-02-13 17:39:47 -0800

    INIT

--- server/requestRoutes.js
+++ server/requestRoutes.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const requests = require('./requests');
-const myMemes = require('../data');
+const myMemes = require('../data/seed.js');
+
+//NOTE1: Please students, never use a nodejs page to store data, not even as a temporary database
+// Get MYSQL and MONGODB servers installed on your computers and set them up, it's not hard; which brings me to my next point:
+
+//MORE IMPORTANT NOTE2!!!:
+// Please have MONGODB and MONGOD command prompt shell installed before your saturday full-stack review!!
 
 module.exports = {
 
